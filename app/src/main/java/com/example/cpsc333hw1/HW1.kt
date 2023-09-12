4dd1f6d63a2b979c0b047ea07a5584a46d3443c2
Lauren-N <rml0729@gmail.com>
2023-09-11 19:53:10 -0700

    intial commit

---
+++ app/src/main/java/com/example/cpsc333hw1/HW1.kt
@@ -0,0 +1,8 @@
+package com.example.cpsc333hw1
+
+/*
+Name: Lauren Nguyen
+Date: 09//11/23
+Assignment: HW1 Random Number Ranking game
+Class: CPSC333
+ */
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CPSC333 HW1"
+include(":app")
+ 
